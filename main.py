9a9599121188d48cd28c3c12045e06dd7a51c049
Audran Semler <audransemler@gmail.com>
2017-12-19 16:27:01 -0500

    renommer data sets

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from mnist import MNIST
+import numpy as np
 
 mndata = MNIST('samples')
 
 images, labels = mndata.load_training()
 
-index = random.randrange(0, len(images))  # choose an index ;-)
-print(mndata.display(images[index]))
+index = np.random.randrange(0, len(images))  # choose an index ;-)
+print(mndata.display(images[0]))
