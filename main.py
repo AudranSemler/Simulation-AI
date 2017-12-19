e7da8b6427ba0b08a77da784e057e2db413fd945
Audran Semler <audransemler@gmail.com>
2017-12-19 16:29:31 -0500

    random index

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ mndata = MNIST('samples')
 
 images, labels = mndata.load_training()
 
-index = np.random.randrange(0, len(images))  # choose an index ;-)
+index = np.random.randint(0, len(images))  # choose an index ;-)
 print(mndata.display(images[0]))
